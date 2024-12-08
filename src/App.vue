d4b0e199a326d5cb98a752bfbb2229e22972450c
Edme - Dawn <nicmaver1337@gmail.com>
2024-12-08 22:11:17 +1000

    feat: Vue Template Added, Tailwind Theme Added, CNAME Added, favicon.ico Added, Placeholder Page Added

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+  <div class="">
+    <div class="container min-h-screen min-w-full flex justify-center items-center bg-primary-800">
+      <h1 class="w text-text-200">Edme's Memory Tools</h1>
+    </div>
+    <div class="container min-h-screen bg-background-300 min-w-full">
+      <div class="ml-10">
+        <h1>Gonna be some stuff</h1>
+        <p>It's gonna be nerdy</p>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+
+</style>
